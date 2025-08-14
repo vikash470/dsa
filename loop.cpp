b2d1fdca2a837db620fb4054c5bc816291be5dac
vikash470 <vikash2304310100135@gmail.com>
2025-08-14 10:13:13 +0530

    aphabet

--- loop.cpp
+++ loop.cpp
@@ -8,14 +8,26 @@
 //     return 0;
 // }
 //using for loop
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int i;
+//     for(i=100;i<=200;i++){
+//         cout<<i<<endl;
+
+//     }
+//     return 0;
+// }
+
+
+//Alphabet print
 #include<iostream>
 using namespace std;
-
 int main(){
-    int i;
-    for(i=100;i<=200;i++){
-        cout<<i<<endl;
-
+    char name;
+    for(name='a';name<='z';name=name+1){
+        cout<<name<<endl;
     }
     return 0;
 }
