954b339174e49e8eec125397caf265aca9dc7555
vikash470 <vikash2304310100135@gmail.com>
2025-08-14 10:19:00 +0530

    print aphabet with space

--- loop.cpp
+++ loop.cpp
@@ -1,33 +1,9 @@
-// #include<iostream>
-// using namespace std;
-
-// int main() {
-//     for(int i = 0; i < 5; i++) {
-//         cout << "Hello, World!" << endl;
-//     }
-//     return 0;
-// }
-//using for loop
-// #include<iostream>
-// using namespace std;
-
-// int main(){
-//     int i;
-//     for(i=100;i<=200;i++){
-//         cout<<i<<endl;
-
-//     }
-//     return 0;
-// }
-
-
-//Alphabet print
 #include<iostream>
 using namespace std;
 int main(){
     char name;
-    for(name='a';name<='z';name=name+1){
-        cout<<name<<endl;
+    for(name='a';name<='z';name++){
+        cout<<name<<" ";
     }
     return 0;
 }
