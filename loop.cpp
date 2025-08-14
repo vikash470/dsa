cfa06bffbb5ae1bfa6f05092a907307868b1f64f
vikash470 <vikash2304310100135@gmail.com>
2025-08-14 09:57:09 +0530

    Update loop.cpp

--- loop.cpp
+++ loop.cpp
@@ -1,1 +1,21 @@
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     for(int i = 0; i < 5; i++) {
+//         cout << "Hello, World!" << endl;
+//     }
+//     return 0;
+// }
+//using for loop
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i;
+    for(i=100;i<=200;i++){
+        cout<<i<<endl;
+
     }
+    return 0;
+}
